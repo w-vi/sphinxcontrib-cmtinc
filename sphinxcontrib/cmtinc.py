58c3a69f1d98bee9787382434558de67f81f0463
wvi <vilibald.wanca@gmail.com>
2014-04-10 13:25:34 +0200

    Add encoding support.
    Minor fixes in README as well.

--- sphinxcontrib/cmtinc.py
+++ sphinxcontrib/cmtinc.py
@@ -12,14 +12,14 @@
 """
 
 import posixpath
+import codecs
 from os import path
+
 from docutils import nodes
 from sphinx.util.nodes import nested_parse_with_titles
 from docutils.statemachine import ViewList
 from docutils.parsers.rst import directives
-from sphinx.application import ExtensionError
 from sphinx.util.compat import Directive
-from sphinx.errors import SphinxError
 import extractor
 
 class CmtIncDirective(Directive):
@@ -43,22 +43,42 @@ class CmtIncDirective(Directive):
                     'a filename argument', line=self.lineno)]
             rel_filename, filename = self.env.relfn2path(self.arguments[0])
             self.env.note_dependency(rel_filename)
+            
+            extr = extractor.Extractor()
+            f = None
             try:
-                extr = extractor.Extractor()
-                with open(filename, 'r') as f:
-                    extr.extract(f)
-                self.content = extr.content
-                self.content_offset = 0
-                # Create a node, to be populated by `nested_parse`.
-                node = nodes.paragraph()
-                # Parse the directive contents.
-                nested_parse_with_titles(self.state, self.content, node)
-                return node.children
-            except (IOError, OSError, extractor.ExtractError):
+                encoding = self.options.get('encoding', self.env.config.source_encoding)
+                codec_info = codecs.lookup(encoding)
+                f = codecs.StreamReaderWriter(open(filename, 'rb'),
+                                                  codec_info[2], codec_info[3], 'strict')
+                extr.extract(f)
+            except (IOError, OSError):
+                return [document.reporter.warning(
+                    'Include file %r not found or reading it failed' % filename,
+                    line=self.lineno)]
+            except UnicodeError:
+                return [document.reporter.warning(
+                    'Encoding %r used for reading included file %r seems to '
+                    'be wrong, try giving an :encoding: option' %
+                    (encoding, filename))]
+            except (extractor.ExtractError):
                 return [self.reporter.error(
                     'External file %r not found or reading '
                     'failed' % filename, line=self.lineno)]
-        return [] 
+            finally:
+                if f is not None:
+                    f.close()
+            self.content = extr.content
+            self.content_offset = 0
+            # Create a node, to be populated by `nested_parse`.
+            node = nodes.paragraph()
+            # Parse the directive contents.
+            nested_parse_with_titles(self.state, self.content, node)
+            return node.children
+        else:
+            return [self.reporter.warning(
+                'include-comment directive needs a filename argument', 
+                line=self.lineno)]
         
 def setup(app):
     app.add_directive('include-comment', CmtIncDirective)
